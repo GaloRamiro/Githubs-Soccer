02de771270dbe24c44bdfa73f278172535bee664
Carlinux89 <carlosj_1912_17@hotmail.com>
2026-06-07 22:45:01 -0500

    cch: mejoras funcionales y visuales del taller mundial 2026

--- mundial.js
+++ mundial.js
@@ -4,46 +4,53 @@ const texto = document.querySelector(".texto-scroll");
 const contenedor = document.querySelector(".contenedor-video");
 
 video.addEventListener("loadedmetadata", () => {
-  window.addEventListener("scroll", () => {
-    const scrollMaximo = contenedor.offsetHeight - window.innerHeight;
+    window.addEventListener("scroll", () => {
+        const scrollMaximo = contenedor.offsetHeight - window.innerHeight;
 
-    const scrollActual = Math.min(window.scrollY, scrollMaximo);
+        const scrollActual = Math.min(window.scrollY, scrollMaximo);
 
-    const progreso = scrollActual / scrollMaximo;
+        const progreso = scrollActual / scrollMaximo;
 
-    // Control del video
-    video.currentTime = progreso * video.duration;
+        // Control del video
+        video.currentTime = progreso * video.duration;
 
-    // Escala del logo
-    const escala = 1 - (progreso * 0.3);
+        // Escala del logo
+        const escala = 1 - (progreso * 0.3);
 
-    logo.style.transform = `scale(${escala})`;
+        logo.style.transform = `scale(${escala})`;
 
-    // Opacidad logo
-    if(progreso < 0.8){
+        // Opacidad logo
+        if (progreso < 0.8) {
 
-    logo.style.opacity = 1 - (progreso * 0.3);
+            logo.style.opacity = 1 - (progreso * 0.3);
 
-}else{
+        } else {
 
-    logo.style.opacity =
-        1 - ((progreso - 0.8) * 5);
+            logo.style.opacity =
+                1 - ((progreso - 0.8) * 5);
 
-}
+        }
 
-    // Opacidad texto
-    texto.style.opacity =
-    Math.max(0, 1 - (progreso * 0.8));
-  });
+        // Opacidad texto
+        texto.style.opacity =
+            Math.max(0, 1 - (progreso * 0.8));
+    });
 });
 
 
 // ---- COPIA ESTO AL FINAL DE TU MUNDIAL.JS ----
 
+const tarjetaGrupoA = document.getElementById('tarjeta-grupo-a');
+
+if (tarjetaGrupoA) {
+    tarjetaGrupoA.addEventListener('click', function () {
+        window.location.href = 'Grupo_A/paginas/grupoA.html';
+    });
+}
 const tarjetaGrupoC = document.getElementById('tarjeta-grupo-c');
 
 if (tarjetaGrupoC) {
-    tarjetaGrupoC.addEventListener('click', function() {
+    tarjetaGrupoC.addEventListener('click', function () {
         window.location.href = 'Grupo_C/paginas/grupoC.html';
     });
 }
@@ -51,7 +58,7 @@ if (tarjetaGrupoC) {
 const tarjetaGrupoB = document.getElementById('tarjeta-grupo-b');
 
 if (tarjetaGrupoB) {
-    tarjetaGrupoB.addEventListener('click', function() {
+    tarjetaGrupoB.addEventListener('click', function () {
         window.location.href = 'Grupo_B/paginas/grupoB.html';
     });
 }
